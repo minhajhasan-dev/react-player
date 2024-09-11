ce5b72b18cfcbc5e8e54a58a5f1fed97677a94b7
minhaj.dev <minhajhasan.dev@gmail.com>
2024-09-12 03:08:43 +0600

    worked on the player slider

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -30,6 +30,12 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
     );
   };
+
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
+
   // State
   const [songInfo, setSongInfo] = useState({
     currentTime: null,
@@ -39,7 +45,13 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     <div className="player">
       <div className="time-control">
         <p> {getTime(songInfo.currentTime)} </p>
-        <input type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type="range"
+        />
         <p> {getTime(songInfo.duration)} </p>
       </div>
       <div className="play-control">
@@ -68,4 +80,4 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 
 export default Player;
 
-// watch from the 10 no video (input slider update)
+// watch from the 11 no video (Error Fixes)
